ee4434d3c6c5aa9112908be19298ff98b7142342
Alimustoofaa <softhouse.ali@gmail.com>
2020-07-08 10:41:01 +0700

    Add Database

--- app/src/main/java/com/alimustofa/textrecognition/database/DbQuestionAndAnswer.java
+++ app/src/main/java/com/alimustofa/textrecognition/database/DbQuestionAndAnswer.java
@@ -6,23 +6,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
 public class DbQuestionAndAnswer extends SQLiteOpenHelper {
-    static abstract class column implements BaseColumns {
-        static final String tableName = "tbl_qna";
-        static final String id = "id";
-        static final String question = "question";
-        static final String answer = "answer";
-    }
-    private static final String DATABASE_NAME = "db_correction.db";
-    private static final int DATABASE_VERSION = 1;
 
-    private static  final String SQL_CREATE_ENTRIES = "CREATE TABLE "+column.tableName+" ("+
+    public static abstract class column implements BaseColumns {
+        public static final String tableName = "tbl_qna";
+        public static final String id = "id";
+        public static final String question = "question";
+        public static final String answer = "answer";
+    }
+    public static final String DATABASE_NAME = "db_correction.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public static  final String SQL_CREATE_ENTRIES = "CREATE TABLE "+column.tableName+" ("+
             column.id+" INTEGER PRIMARY KEY  AUTOINCREMENT,"+
             column.question+" TEXT NOT NULL,"+
             column.answer+" TEXT NOT NULL)";
 
-    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS "+column.tableName;
+    public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS "+column.tableName;
 
-    DbQuestionAndAnswer(Context context){
+     public DbQuestionAndAnswer(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
